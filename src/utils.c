11dbcf5af95a951b6d928c2c4f40ffc78c5c5e21
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2024-01-23 12:48:58 +0100

    Norminette & Update 2^64 & 2^32

---
+++ src/utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsaiago- <bsaiago-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 19:52:48 by bsaiago-          #+#    #+#             */
+/*   Updated: 2024/01/23 12:12:47 by bsaiago-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	long	sign;
+	long	nb;
+
+	sign = 1;
+	nb = 0;
+	while (*str == '\t' || *str == '\v' || *str == '\f' || \
+		*str == '\n' || *str == ' ' || *str == '\r')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -sign;
+		str++;
+	}
+	while (*str >= MIN_DIGIT_CHAR && *str <= MAX_DIGIT_CHAR)
+	{
+		nb = nb * 10 + *str - '0';
+		str++;
+	}
+	return (nb * sign);
+}
+
+void	free_stack(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = *stack;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(stack);
+}
+
+int	is_sorted(t_list **stack)
+{
+	t_list	*head;
+
+	head = *stack;
+	while (head && head->next)
+	{
+		if (head->content > head->next->content)
+			return (0);
+		head = head->next;
+	}
+	return (1);
+}
+
+int	ft_strlen_nb(char *str)
+{
+	int	len;
+	int	start;
+
+	len = 0;
+	start = 0;
+	while (str[start] == '0' && str[start])
+		start++;
+	while (str[start++])
+		len++;
+	return (len);
+}
